7cf0bcab94c8df2782acdeebd085d0f9adc370d5
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-09-13 16:57:44 +0530

    Update retPermutationsOfString.cpp

--- retPermutationsOfString.cpp
+++ retPermutationsOfString.cpp
@@ -23,6 +23,8 @@ cab
 cba
 */
 
+//SOLUTION.H
+
 #include <string>
 using namespace std;
 
@@ -49,3 +51,26 @@ int returnPermutations(string input, string output[]){
     
     return k;
 }
+
+//MAIN.CPP
+
+#include <iostream>
+#include <string>
+#include "solution.h"
+using namespace std;
+
+int main(){
+    string input;
+    cin >> input;
+    
+    string output[10000];
+    
+    int count = returnPermutations(input, output);
+    
+    for(int i = 0; i < count && i < 10000; i++){
+        cout << output[i] << endl;
+    }
+    
+    return 0;
+}
+
