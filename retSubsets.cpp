6bc404357081bcb70f6c1d59c476510d6ff82060
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-09-15 23:03:43 +0530

    Update retSubsets.cpp

--- retSubsets.cpp
+++ retSubsets.cpp
@@ -1,3 +1,35 @@
+/*
+Return subset of an array
+
+Given an integer array (of length n), find and return all the subsets of input array.
+Subsets are of length varying from 0 to n, that contain elements of the array. But the 
+order of elements should remain same as in the input array.
+
+Note : The order of subsets are not important.
+
+Input format :
+
+Line 1 : Size of array
+Line 2 : Array elements (separated by space)
+
+Sample Input:
+
+3
+15 20 12
+
+Sample Output:
+
+[] (this just represents an empty array, don't worry about the square brackets)
+12 
+20 
+20 12 
+15 
+15 12 
+15 20 
+15 20 12 
+
+*/
+
 /***
 You need to save all the subsets in the given 2D output array. And return the number of subsets(i.e. number of rows filled in output) from the given function.
 
